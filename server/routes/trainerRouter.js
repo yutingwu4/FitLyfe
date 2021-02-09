876f20fbc2796c2c09c8106054c73bd9e36a67cc
Will Kencel <wkencel1@gmail.com>
2021-02-09 10:49:35 -0800

    Routing set up

--- server/routes/trainerRouter.js
+++ server/routes/trainerRouter.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const TrainerController = require('../controllers/trainerController.js')
+const TrainerController = require('../../controllers/trainerController.js')
 
+
+//set up error handling
 // get request to /getTrainers and use the trainer controller to set the trainers on the response object
 router.get("/getTrainers",  TrainerController.getTrainers, (req, res) => {
   return res.send(res.locals.allTrainers)
