d0923353d324db336a0f3a4a665b1c4a21f84d76
Will Kencel <wkencel1@gmail.com>
2021-02-08 21:56:37 -0800

    took out console.log

--- server/controllers/trainerController.js
+++ server/controllers/trainerController.js
@@ -4,7 +4,6 @@ const pool = require('../models/TrainerModel')
 const TrainerController = {
   // async function to database to place rows on the response object
   async getTrainers(req, res, next) {
-    console.log('in controller')
     const results = await pool.query("SELECT * FROM players")
     res.locals.allTrainers = results.rows
     return next()
