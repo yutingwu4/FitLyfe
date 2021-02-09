1acc047dd3b09ad307dfdbe2a5ee3570ad976820
Kevin Ngo <kevinngo.cal@gmail.com>
2021-02-09 12:21:05 -0800

    Add initial base frontend components (Client Card, Form, and Info)
    - [x] New feature (change which adds functionality)
    
    - Set up base frontend (without logic to connect to backend yet)
    
    - Use React Hooks and React Hooks Forms to create initial components
    
    Co-authored-by: Kevin Ngo <kevinngo.cal@gmail.com>
    Co-authored-by: Julie Wu <yutingwu.studio@gmail.com>
    Co-authored-by: Navi Dhillon <navdip.dhillon.91@gmail.com>
    Co-authored-by: Will Kencel <wkencel1@gmail.com>
    Co-authored-by: Mario Eldin <marioeldin@gmail.com>

--- client/components/ClientInfo.jsx
+++ client/components/ClientInfo.jsx
@@ -6,15 +6,75 @@ function ClientInfo() {
   const onSubmit = (data) => console.log(data);
 
   return (
-    <form
-      style={{ display: 'flex', flexDirection: 'column' }}
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <input name="exampleRequired" ref={register({ required: true })} />
-      {errors.exampleRequired && <span>This field is required</span>}
-      <input type="submit" />
-    </form>
+    <div>
+      <div className="clientInfo__left">
+        {/* Client Photo */}
+        {/* For the time-being we would just store dummy data image. */}
+        <img src="#" />
+        {/* Name of Client */}
+        <p id="name">Wanda</p>
+        <p id="age">Age: </p>
+      </div>
+
+      <div className="clientInfo__leftStats">
+        {/* Gender */}
+        <p id="clientGender">Gender: </p>
+        {/* Current Weight */}
+        <p id="currentWeight">Current Weight: </p>
+        {/* Goal Weight */}
+        <p id="goalWeight">Goal Weight: </p>
+        {/* Macro Goals */}
+        <p id="macroGoal">Macro Goal: </p>
+        {/* Nutrional Goals/Calorie */}
+        <p id="nutritionalGoal">Nutritional Goals: </p>
+      </div>
+      <div>
+        {/* Client Info FORM */}
+        <form
+          style={{ display: 'flex', flexDirection: 'column' }}
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <label>
+            {' '}
+            Water Intake:
+            <input
+              name="waterIntake"
+              type="number"
+              ref={register({ required: true })}
+            />
+            ozs.
+          </label>
+
+          <label>
+            {' '}
+            Daily Macro Goals:
+            <input
+              name="DMG"
+              type="checkbox"
+              ref={register({ required: true })}
+            />
+          </label>
+
+          <label>
+            {' '}
+            Calorie Intake:
+            <input
+              name="calorieIntake"
+              type="number"
+              ref={register({ required: true })}
+            />
+          </label>
+
+          <input type="submit" />
+        </form>
+      </div>
+      {/* Water Intake oz*/}
+      {/* Daily Marco Goals checkbox if hit*/}
+      {/* Calorie Intake  */}
+      {/* SAVE */}
+    </div>
+
+    // add back button functionality
   );
 }
-
 export default ClientInfo;
