d629b2c3ce591741e67d5853aec5464052d2e73b
Will Kencel <wkencel1@gmail.com>
2021-02-08 16:16:17 -0800

    connected front and backend for starting point

---
+++ client/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const whereToPlace = document.getElementById('root');
+
+ReactDOM.render(
+     <div><h1>hi will</h1>
+    </div>, whereToPlace);
