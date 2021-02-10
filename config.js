8bd0f98bec7b057920994da7c9d8fae9429907af
yutingwu4 <scorp.only@gmail.com>
2021-02-09 18:56:32 -0800

    commit before pull from marwan feature branch for routing

--- config.js
+++ config.js
@@ -1,11 +1,4 @@
 const config = {
-<<<<<<< HEAD
-  URI:
-    "postgres://qorrigcd:rsdscEqZMtGj_4RSMwSNVUPPHMTONyCy@ziggy.db.elephantsql.com:5432/qorrigcd",
-};
-module.exports = config;
-=======
     URI: 'postgres://dfqpfaet:GigyWQsljfTAXNLHbXTY62dWSgN2jTRm@ziggy.db.elephantsql.com:5432/dfqpfaet',
   };
   module.exports = config;
->>>>>>> 876f20fbc2796c2c09c8106054c73bd9e36a67cc
