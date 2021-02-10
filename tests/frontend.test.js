ec94f8bd6cb73e835dbb2fc5ed77977df337a1d6
Will Kencel <wkencel1@gmail.com>
2021-02-10 12:06:17 -0800

    test folder and script

---
+++ tests/frontend.test.js
@@ -0,0 +1,8 @@
+// describes what the test is
+describe('jest testing is in progress', () => {
+  // what the specific test is
+    it('jest testing activated', () => {
+      // what is expected
+      expect(true).toBeTruthy();
+    })
+  }) 
